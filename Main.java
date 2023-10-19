f54755b2c0b0caaa2dbc724d819f1ec39e85ef0e
AjayKumarChawla <ajaychawla804@gmail.com>
2023-10-20 00:29:40 +0200

    SortedArray

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import Array101.MaxConsecutiveOnes;
 import Array101.NumbersWithEvenNumberOfDigits;
+import Array101.SquaresofSortedArray;
 
 class Main {
     public static void main(String args[]) {
