84957273487f911e3f33014820caf541fa6ac51d
AjayKumarChawla <ajaychawla804@gmail.com>
2023-10-21 00:55:19 +0200

    added duplicate zeros solutions

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import Array101.MaxConsecutiveOnes;
 import Array101.NumbersWithEvenNumberOfDigits;
 import Array101.SquaresofSortedArray;
+import Array101.DuplicateZeros;
 
 class Main {
     public static void main(String args[]) {
