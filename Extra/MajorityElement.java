28ce6a7faf94dc1eb5882b7892e6c86062cc2182
ajaykumarchawla <ajaychawla804@gmail.com>
2024-10-24 19:05:57 +0200

    added MajorityElement

--- Extra/MajorityElement.java
+++ Extra/MajorityElement.java
@@ -36,4 +36,20 @@ public class MajorityElement {
         }
         return current;
     }
+
+    public int majorityElementOptimal(int[] nums) {
+        int count = 0, element = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                element = nums[i];
+                count = 1;
+            } else if (element == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return element;
+    }
 }
