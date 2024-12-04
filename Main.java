ed886ab569bee4401a8e8b9f0bcb42565152ccaa
ajaykumarchawla <ajaychawla804@gmail.com>
2024-12-04 23:44:23 +0100

    updating main to work with queue

--- Main.java
+++ Main.java
@@ -149,15 +149,15 @@ class Main {
         // System.out.println(myStack.pop()); // return 2
         // System.out.println(myStack.empty()); // return False
 
-        // QueueUsingStack myQueue = new QueueUsingStack();
-        // myQueue.push(1); // queue is: [1]
-        // myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
-        // myQueue.push(3); // queue is: [1, 2] (leftmost is front of the queue)
+        QueueUsingStack myQueue = new QueueUsingStack();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        myQueue.push(3); // queue is: [1, 2] (leftmost is front of the queue)
 
-        // System.out.println(myQueue.peek()); // return 1
-        // System.out.println(myQueue.pop()); // return 1, queue is [2]
-        // myQueue.pop();
-        // System.out.println(myQueue.empty()); // return false
+        System.out.println(myQueue.peek()); // return 1
+        System.out.println(myQueue.pop()); // return 1, queue is [2]
+        myQueue.pop();
+        System.out.println(myQueue.empty()); // return false
 
         MinStack minStack = new MinStack();
         minStack.push(-2);
