6f948df1afe847be4672ddfebe2bbaa48ed83a95
ajaykumarchawla <ajaychawla804@gmail.com>
2024-12-27 21:59:15 +0100

    number of enclaves

---
+++ Extra/NumberOfEnclave.java
@@ -0,0 +1,65 @@
+package Extra;
+// 020. Number of Enclaves
+
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+
+// A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+
+// Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+
+// Example 1:
+
+// Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+// Output: 3
+// Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
+// Example 2:
+
+// Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
+// Output: 0
+// Explanation: All 1s are either on the boundary or can reach the boundary.
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 500
+// grid[i][j] is either 0 or 1.
+public class NumberOfEnclave {
+    public void dfs(int[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+
+    }
+
+    public int numEnclaves(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, grid[0].length - 1);
+        }
+        for (int j = 0; j < grid[0].length; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, grid.length - 1, j);
+        }
+        int count = 0;
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
