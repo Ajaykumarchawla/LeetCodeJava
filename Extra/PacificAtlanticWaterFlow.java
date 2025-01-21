4a0893f7eadb62c6c3fce0956cd2e09dfe592a3c
ajaykumarchawla <ajaychawla804@gmail.com>
2025-01-21 18:50:11 +0100

    added pacific atlantic water flow

---
+++ Extra/PacificAtlanticWaterFlow.java
@@ -0,0 +1,111 @@
+package Extra;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+// 417. Pacific Atlantic Water Flow
+// Solved
+// Medium
+// Topics
+// Companies
+// There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean. The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+
+// The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+
+// The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height. Water can flow from any cell adjacent to an ocean into the ocean.
+
+// Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+
+// Example 1:
+
+// Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+// Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+// Explanation: The following cells can flow to the Pacific and Atlantic oceans, as shown below:
+// [0,4]: [0,4] -> Pacific Ocean 
+//        [0,4] -> Atlantic Ocean
+// [1,3]: [1,3] -> [0,3] -> Pacific Ocean 
+//        [1,3] -> [1,4] -> Atlantic Ocean
+// [1,4]: [1,4] -> [1,3] -> [0,3] -> Pacific Ocean 
+//        [1,4] -> Atlantic Ocean
+// [2,2]: [2,2] -> [1,2] -> [0,2] -> Pacific Ocean 
+//        [2,2] -> [2,3] -> [2,4] -> Atlantic Ocean
+// [3,0]: [3,0] -> Pacific Ocean 
+//        [3,0] -> [4,0] -> Atlantic Ocean
+// [3,1]: [3,1] -> [3,0] -> Pacific Ocean 
+//        [3,1] -> [4,1] -> Atlantic Ocean
+// [4,0]: [4,0] -> Pacific Ocean 
+//        [4,0] -> Atlantic Ocean
+// Note that there are other possible paths for these cells to flow to the Pacific and Atlantic oceans.
+// Example 2:
+
+// Input: heights = [[1]]
+// Output: [[0,0]]
+// Explanation: The water can flow from the only cell to the Pacific and Atlantic oceans.
+
+// Constraints:
+
+// m == heights.length
+// n == heights[r].length
+// 1 <= m, n <= 200
+// 0 <= heights[r][c] <= 105
+public class PacificAtlanticWaterFlow {
+    public void findFlow(int i, int j, int[][] ocean, int[][] height, int digit) {
+        if (i >= 0 && i < height.length && j >= 0 && j < height[0].length) {
+            if (ocean[i][j] == 0 && digit <= height[i][j]) {
+                ocean[i][j] = 1;
+                findFlow(i - 1, j, ocean, height, height[i][j]);
+                findFlow(i + 1, j, ocean, height, height[i][j]);
+
+                findFlow(i, j - 1, ocean, height, height[i][j]);
+                findFlow(i, j + 1, ocean, height, height[i][j]);
+
+            }
+        }
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int[][] pacific = new int[heights.length][heights[0].length];
+        int[][] atlantic = new int[heights.length][heights[0].length];
+
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = 0; j < heights[0].length; j++) {
+                if ((i == 0 || j == 0) && pacific[i][j] == 0) {
+                    findFlow(i, j, pacific, heights, -1);
+                }
+
+                if (i == heights.length - 1 || j == heights[0].length - 1 && atlantic[i][j] == 0) {
+                    findFlow(i, j, atlantic, heights, -1);
+
+                }
+            }
+        }
+
+        for (int[] x : pacific)
+            System.out.println(Arrays.toString(x));
+
+        for (int[] x : atlantic)
+            System.out.println(Arrays.toString(x));
+        List<List<Integer>> list = new LinkedList<>();
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = 0; j < heights[0].length; j++) {
+                if (pacific[i][j] == 1 && atlantic[i][j] == 1) {
+                    List<Integer> newList = new LinkedList<>();
+                    newList.add(i);
+                    newList.add(j);
+                    list.add(newList);
+                }
+            }
+        }
+        return list;
+    }
+
+}
+
+// test cases
+// [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+// [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
+// [[1]]
+// [[1,2],[2,2]]
+// [[1,2],[1,2]]
+// [[1,2,3],[8,9,4],[7,6,5]]
+// [[11,2,11,0,15,12,4,15,0,14,11,3,19,11,5,11,18,19,4,3,11,1,9,17,5,2,15,18,11,15],[12,10,8,15,4,7,4,5,7,8,5,12,3,3,10,12,16,15,17,13,13,16,0,0,17,17,11,3,14,0],[8,18,1,6,15,16,14,11,9,11,3,4,17,7,2,16,18,2,0,0,16,18,10,15,14,18,10,19,17,6],[14,17,4,13,13,6,16,1,3,18,18,18,4,1,15,4,0,9,19,3,6,7,19,13,11,11,10,19,3,15],[16,6,19,17,19,17,5,12,6,3,1,0,3,10,13,18,4,3,9,0,1,18,9,15,18,3,4,6,1,15],[1,2,12,9,9,7,17,0,1,14,18,1,5,3,0,7,2,19,7,19,1,11,1,3,2,4,0,3,16,18],[18,10,10,3,12,11,7,8,3,16,7,11,11,12,15,1,13,9,8,17,1,9,7,19,1,14,8,10,18,14],[5,19,9,4,10,14,1,5,11,16,11,3,5,4,19,8,11,16,19,12,6,3,18,16,17,8,11,19,7,14],[0,15,17,11,10,13,19,0,10,3,15,19,3,3,3,4,3,12,17,10,5,16,12,5,5,17,5,17,6,6],[8,19,9,3,13,8,13,17,4,12,13,8,13,12,10,10,16,7,2,8,17,3,7,1,7,16,11,19,13,19],[6,19,6,13,10,5,14,7,3,1,10,6,4,8,15,0,0,2,12,13,14,14,7,5,1,16,15,15,4,7],[7,7,11,14,2,4,14,2,2,0,6,11,15,14,11,13,2,3,14,9,16,3,8,15,2,18,15,15,2,2],[7,5,12,10,14,3,6,9,2,1,2,15,0,4,7,9,7,12,15,9,2,13,7,8,7,9,4,3,5,19],[11,9,1,8,0,15,1,6,5,11,14,19,6,11,0,12,1,6,8,7,0,1,2,9,14,4,5,8,3,16],[8,0,11,5,14,4,19,0,6,8,1,10,13,8,18,6,6,4,5,9,10,14,14,13,12,16,4,3,3,11],[0,9,6,19,16,4,5,10,13,19,8,15,14,7,13,11,17,18,14,18,19,11,0,4,12,11,2,8,17,14],[16,19,16,9,9,14,5,13,7,10,18,6,15,12,12,1,11,16,1,8,1,7,16,7,19,6,12,0,15,0],[2,4,18,15,13,9,4,18,19,5,16,7,10,1,7,7,4,4,10,8,13,15,9,4,16,13,6,3,13,7],[3,11,10,13,6,4,0,13,11,4,5,6,19,13,8,10,8,9,2,4,4,11,12,8,12,15,6,1,10,12],[7,6,19,3,2,14,15,6,9,1,6,14,4,15,13,9,14,7,10,12,17,18,6,4,12,4,1,6,6,12],[15,17,9,15,9,15,9,10,10,11,12,17,2,18,11,0,6,11,14,17,2,13,9,13,3,4,3,1,8,11],[17,13,12,17,4,19,19,7,7,13,19,10,4,16,1,18,14,2,9,18,2,8,3,1,10,9,12,6,2,11],[17,12,6,8,3,16,5,2,16,3,13,3,13,9,11,11,5,12,14,16,3,19,16,16,1,14,5,3,17,19],[1,4,0,3,1,17,5,15,2,19,12,7,18,13,1,0,7,2,9,18,10,18,8,9,13,13,8,10,14,14],[9,14,4,18,10,18,3,9,9,17,16,4,19,7,3,18,7,0,10,13,9,10,11,16,3,5,1,2,16,19],[8,10,13,8,7,2,9,4,16,15,5,4,15,7,9,7,15,2,6,17,14,3,13,3,4,15,13,10,8,16],[17,7,19,19,13,12,6,0,11,4,10,4,1,9,15,9,7,7,14,6,7,18,9,13,6,16,5,2,17,1],[2,7,0,4,8,18,4,11,13,4,11,12,3,18,11,2,4,18,3,3,17,9,18,11,9,15,14,19,7,17],[13,1,15,18,4,12,18,18,15,16,7,17,9,15,11,3,9,7,18,13,3,11,7,19,10,10,7,13,7,19],[17,17,14,3,19,7,1,13,9,3,6,16,10,8,14,8,17,18,12,11,4,11,10,15,9,0,4,12,7,15],[4,4,8,1,7,11,13,4,11,5,18,2,16,11,16,13,0,13,13,12,11,15,8,4,0,3,2,9,8,15],[17,4,13,5,3,17,14,4,7,6,6,11,16,18,2,0,3,12,1,5,12,16,3,14,4,16,5,8,15,9],[5,3,17,17,6,4,19,5,4,6,11,4,14,18,4,19,16,15,1,17,3,8,13,14,16,13,18,19,6,4],[15,0,8,15,6,6,11,8,18,2,4,10,18,16,15,8,1,5,9,13,7,19,12,2,9,18,1,15,12,8],[5,0,18,14,1,8,18,15,5,13,15,7,8,8,9,0,14,12,4,17,2,10,9,7,19,7,19,9,7,1],[7,4,16,16,13,4,3,6,15,11,14,7,3,0,5,15,10,13,18,18,11,6,7,9,19,13,4,2,7,9],[9,14,15,11,14,5,15,1,19,15,3,4,0,10,4,1,2,15,18,15,15,2,9,0,3,10,9,16,4,1],[14,13,17,19,0,13,15,9,16,18,5,6,16,16,6,10,14,15,17,5,9,2,5,11,19,19,11,6,15,14],[17,7,19,6,5,19,10,2,11,17,17,13,16,13,19,4,12,3,4,13,7,9,19,9,12,3,16,8,18,13]]
